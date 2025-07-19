90de685a8daf47712d45644e3594686b66dd6a82
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-07-19 23:46:00 +0530

    update vscode settings

--- settings/home-settings/vscode.nix
+++ settings/home-settings/vscode.nix
@@ -1,6 +1,44 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  extensions = with pkgs.vscode-extensions; [
+    ms-toolsai.jupyter
+    ms-python.python
+    ms-python.vscode-pylance
+    wakatime.vscode-wakatime
+  ];
+in {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhs;
+    profiles = {
+      "hitmonlee" = {
+        enableUpdateCheck = false;
+        userSettings = {
+          "github.copilot.enable" = {
+            "*" = false;
+            "plaintext" = false;
+            "markdown" = false;
+            "scminput" = false;
+            "chat.editor.fontFamily" = "FiraCode Nerd Font";
+          };
+          "files.autoSave" = "afterDelay";
+          "editor.fontFamily" = "FiraCode Nerd Font Propo";
+        };
+        extensions = extensions;
+      };
+      "default" = {
+        enableUpdateCheck = false;
+        userSettings = {
+          "github.copilot.enable" = {
+            "*" = false;
+            "plaintext" = false;
+            "markdown" = false;
+            "scminput" = false;
+            "chat.editor.fontFamily" = "FiraCode Nerd Font";
+          };
+          "files.autoSave" = "on";
+        };
+        extensions = extensions;
+      };
+    };
   };
 }

b4c79eb5739b4961032889dcf1f8187973442353
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-07-24 12:33:47 +0530

    changed wallpaper and some minor change in nvim config

--- settings/home-settings/stylix/stylix.nix
+++ settings/home-settings/stylix/stylix.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.image = ../../../wallpapers/b-061.jpg;
+  stylix.image = ../../../wallpapers/b-345.jpg;
   stylix.polarity = "dark";
   stylix.targets.starship.enable = false;
   stylix.targets.fish.enable = false;
@@ -23,4 +23,26 @@
     desktop = 0.2;
   };
   stylix.fonts.sizes.terminal = 16;
+
+  stylix.fonts = {
+    serif = {
+      package = pkgs.nerd-fonts.fira-code;
+      name = "FiraCode Nerd Font";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.nerd-fonts.fira-code;
+      name = "FiraCode Nerd Font Mono";
+    };
+
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
 }

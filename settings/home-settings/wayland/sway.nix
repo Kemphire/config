a584a03e480bf835ed4d95d097f9b5559b093a91
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-07-26 00:53:33 +0530

    minor tweak

--- settings/home-settings/wayland/sway.nix
+++ settings/home-settings/wayland/sway.nix
@@ -276,7 +276,7 @@
           timeout 500 'systemctl suspend' \
           before-sleep 'swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2' \
           after-resume 'swaymsg "output * power on"' \
-          lock 'swaymsg "output * power off"; swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2' \
+          lock 'swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2' \
           unlock 'swaymsg "output * power on"'
         '';
         extraConfig = let
